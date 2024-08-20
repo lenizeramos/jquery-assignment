eabcfa7fb6f2c7d575f783cadbb50f85e3b64865
lenizeramos <lenizemr@gmail.com>
2024-08-19 19:22:07 -0700

    completed

--- src/js/index.js
+++ src/js/index.js
@@ -9,44 +9,116 @@ $(() => {
     type: "GET",
   });
 
-  $.when(postsRequest, commentsRequest)
-    .done(function (postsReponse, commentsResponse) {
-      let posts = [];
-      console.log(postsReponse[0]);
-      postsReponse[0].forEach((post) => {
-        let comments = commentsResponse[0].filter((comment) => {
-          return post.id === comment.postId;
-        });
-        post["comments"] = comments;
-        posts.push(post);
-      });
+  const usersRequest = $.ajax({
+    url: "https://jsonplaceholder.typicode.com/users",
+    type: "GET",
+  });
 
-      // console.log(posts.length);
+  $.when(postsRequest, commentsRequest, usersRequest)
+    .done(function (postsReponse, commentsResponse, usersResponse) {
+      let posts = buildPosts(
+        postsReponse[0],
+        commentsResponse[0],
+        usersResponse[0]
+      );
 
-      posts.forEach((post) => {
-        let postTitle = post.title;
-        let postBody = post.body;
-        console.log(post.comments);
-
-        let commentBody = post.comments.map((comment) => {
-          return comment.body;
-        });
-        console.log(commentBody);
-
-        let h3PostTitle = $("<h3>").html(postTitle);
-
-        let pPostBody = $("<p>").html(postBody);
-
-        /* let pCommentBody = $("<p>").html(commentBody) */
-
-        let divPost = $("<div>", {
-          class: "post",
-        }).append(h3PostTitle, pPostBody);
-
-        $("#posts").append(divPost);
-      });
+      buildHTML(posts);
     })
     .fail(function (error) {
       console.error("Error:", error);
     });
+
+  function buildPosts(postsData, commentsData, usersData) {
+    let posts = [];
+    postsData.forEach((post) => {
+      let comments = commentsData.filter((comment) => {
+        return post.id === comment.postId;
+      });
+      post["comments"] = comments;
+
+      let user = usersData.find((user) => user.id === post.userId);
+      post["user"] = user;
+
+      posts.push(post);
+    });
+
+    return posts;
+  }
+
+  function buildHTML(posts) {
+    posts.forEach((post) => {
+      let postUser = post.user.name;
+      let postTitle = post.title;
+      let postBody = post.body;
+
+      let h3PostUser = $("<h3>").html(`Posted by: ${postUser}`);
+      let h4PostTitle = $("<h4>").html(`Title: ${postTitle}`);
+      let pPostBody = $("<p>").html(`Body: ${postBody}`);
+
+      let divComment = $("<div>", { style: "display: none" });
+      let divParagraphComments = $("<div>");
+      let commentButton = $("<button>", {
+        class: "toggleCommentsButton",
+        text: "Comments",
+      });
+
+      for (const comment of post.comments) {
+        let pCommentBody = $("<p>", { class: "comments " }).html(comment.body);
+
+        divParagraphComments.append(pCommentBody);
+      }
+
+      divComment.append(divParagraphComments);
+
+      let addCommentButton = $("<button>", {
+        class: "addCommentButton",
+        text: "Add a comment",
+      });
+      divComment.append(addCommentButton);
+
+      let form = $("<form>", {
+        style: "display: none",
+      });
+      let textArea = $("<textarea>", {
+        placeholder: "Type your comment here. Max 150 characters",
+        maxlength: 150,
+      });
+      let submitButton = $("<button>", {
+        type: "submit",
+        text: "Submit",
+      });
+      form.append(textArea);
+      form.append(submitButton);
+
+      divComment.append(form);
+
+      let divPost = $("<div>", {
+        class: "post",
+      })
+        .append(h3PostUser, h4PostTitle, pPostBody)
+        .append(commentButton)
+        .append(divComment);
+
+      $("#posts").append(divPost);
+
+      commentButton.on("click", function () {
+        divComment.slideToggle();
+      });
+
+      addCommentButton.on("click", function () {
+        form.slideToggle();
+        form.on("submit", function (event) {
+          event.preventDefault();
+
+          let msgValue = textArea.val().trim();
+          let pCommentBody = $("<p>", { class: "comments " }).html(msgValue);
+          if (msgValue != "") {
+            divParagraphComments.append(pCommentBody);
+          }
+
+          textArea.val("");
+        });
+      });
+    });
+  }
 });
